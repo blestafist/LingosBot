1293876410bfc6bd5b1f0aaa7f5e25b3d9d2424d
blestafist <pestit@proton.me>
2025-10-14 11:04:45 +0200

    Added database reading and WebDriverWait

--- Bot.cs
+++ Bot.cs
@@ -13,8 +13,50 @@ using OpenQA.Selenium.Safari;
 namespace LingosBot
 {
 
-    public class Bot
+    internal class Bot
     {
+        public static Config config = ConfigDataBaseTweaks.GetConfig(); // getting user config
+        public static IWebDriver webDriver = Helpers.GetWebDriver();
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(config.email);
 
+            Console.WriteLine("Entry point");
+        }
+    }
+
+    public static class Helpers
+    {
+        public static IWebElement WaitForElement(By by, short timeout = 10, Func<IWebDriver, IWebElement>? function = null)
+        {
+            WebDriverWait wait = new(Bot.webDriver, TimeSpan.FromSeconds(timeout));
+            if (function != null)
+            {
+                return wait.Until(function);
+            }
+
+            return wait.Until(ExpectedConditions.ElementIsVisible(by));
+        }
+        
+        public static IWebDriver GetWebDriver()
+        {
+            return Enum.Parse<AvailibleDrivers>(Bot.config.browser) switch
+            {
+                AvailibleDrivers.Chrome => new ChromeDriver(),
+                AvailibleDrivers.Edge => new EdgeDriver(),
+                AvailibleDrivers.Firefox => new FirefoxDriver(),
+                AvailibleDrivers.Safari => new SafariDriver(),
+                _ => new ChromeDriver(),
+            };
+        }
+    }
+
+
+    enum AvailibleDrivers : byte
+    {
+        Firefox,
+        Edge,
+        Chrome,
+        Safari
     }
 }
--- DataBase.cs
+++ DataBase.cs
@@ -1,21 +1,42 @@
-using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Net.Quic;
+using Newtonsoft.Json;
+using OpenQA.Selenium.DevTools.V138.Network;
 
 namespace LingosBot
 {
 
-    public class ConfigDataBase
+    public class Config
     {
         public string email = "ENTER YOUR LINGOS EMAIL HERE";
         public string password = "ENTER YOUR LINGOS PASSWORD HERE";
         public int numberOfLessons = 1; // number of lessons to do
+        public string browser = "Chrome";
+
+        public string wordsDataBasePath = "words.json";
     }
-    
+
     public class WordsDataBase
     {
         public Dictionary<string, string> words = []; // key is an original word, value is a translated word
+    }
+    
+    public static class ConfigDataBaseTweaks
+    {
+        private const string filePath = "config.json";
+        public static Config GetConfig()
+        {
+
+            if (!File.Exists(filePath)) {
+                File.WriteAllText(filePath, JsonConvert.SerializeObject(new Config(), Formatting.Indented));
+
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Enter appropriate values for you in config.json file and relaunch the program! ");
+                
+                Environment.Exit(0); // quit the program
+            }
+            
+            Config config = JsonConvert.DeserializeObject<Config>(File.ReadAllText(filePath));
+            return config ?? new Config();
+        }
     }
 }
