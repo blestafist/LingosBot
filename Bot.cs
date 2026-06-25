a21147259474ee215c03e475a309ebe7cf14684d
ufw <claudaiku@pm.me>
2026-06-25 22:50:28 +0200

    Update Bot.cs to use the new modules

--- Bot.cs
+++ Bot.cs
@@ -9,6 +9,7 @@ namespace LingosBot
     internal class Bot
     {
         public static Config config = ConfigDataBaseTweaks.GetConfig(); // getting user config
+        public static AppConfig appConfig = new AppConfig(config);
         public static WordsDataBaseTweaks dataBase = new();
         public static IWebDriver webDriver = new BrowserFactory().Create(config);
         public static Random rnd = new(); // random generator for making errors with chance
@@ -20,11 +21,12 @@ namespace LingosBot
             else if (OperatingSystem.IsLinux()) { Console.WriteLine("LINUX USER :)))"); }
 
 
-            webDriver.Navigate().GoToUrl("https://lingos.pl/h/login"); // go to lingos url
-            SeleniumMethods.Login();  // logging in
-            dataBase.InitDB(); // Initialising words DB
+            webDriver.Navigate().GoToUrl(appConfig.LoginUrl);
+            var loginService = new LoginService(webDriver, appConfig);
+            loginService.Login();
+            dataBase.InitDB();
 
-            for (int i = 0; i < config.numberOfLessons; i++)
+            for (int i = 0; i < appConfig.NumberOfLessons; i++)
             {
                 SeleniumMethods.LaunchLesson();
 
@@ -37,44 +39,9 @@ namespace LingosBot
 
 
 
-    internal static class SeleniumMethods // a static class for selenium methods, such as Login(), DoLesson() and others
+    internal static class SeleniumMethods
     {
-        
-        public static void Login() // login to Lingos
-        {
-            if (Bot.config.automaticLogin)
-            {
-                try
-                {
-                    string login = Bot.config.email; // reading config login
-                    string password = Bot.config.password;  // and password
-
-                    var declineCookies = Helpers.WaitForElement(By.Id("CybotCookiebotDialogBodyButtonDecline"), 15, ExpectedConditions.ElementToBeClickable(By.Id("CybotCookiebotDialogBodyButtonDecline")));
-                    declineCookies.Click(); // decline fucking cookies
-
-                    var loginBox = Helpers.WaitForElement(By.Name("login"), 10, ExpectedConditions.ElementToBeClickable(By.Name("login")));
-                    var passwordBox = Bot.webDriver.FindElement(By.Name("password"));
-                    var submitButton = Bot.webDriver.FindElement(By.Id("submit-login-button")); // find elemets of login
-
-                    loginBox.SendKeys(login); // enter login
-                    passwordBox.SendKeys(password); // enter password
-                    ((IJavaScriptExecutor)Bot.webDriver).ExecuteScript("arguments[0].click();", submitButton); // click button via JS (element to be clickable)
-                }
-
-                catch (Exception e) // exc
-                {
-                    Console.WriteLine("Error while logging in: " + e.Message);
-                }
-            }
-
-            else // if user wants no auto login
-            {
-                Console.WriteLine("Login to Lingos and press enter...");
-                Console.ReadLine();
-            }
-        }
-
-        public static void LaunchLesson() // launch lesson
+        public static void LaunchLesson()
         {
             try
             {
